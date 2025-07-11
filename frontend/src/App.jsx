37a9040af0806f1de8459a5e3063651a0efa26e3
brylle60 <johnbryllecrodua77@gmail.com>
2025-07-11 20:53:21 +0800

    changes between files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
-import TaskList from './Components/TaskList';
-
 
 // testing the main components of the app api from backend
 // This is the main entry point of the React application
+import './App.css';
+import TaskList from './Components/TaskList';
+
+//this is the main entry point for the React application
 function App(){
     return (
         <div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
 // the main entry point for the React application
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
